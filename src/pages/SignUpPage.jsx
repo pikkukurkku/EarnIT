8c5973e561557b91089f992fed6e6f280b8c98e5
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-29 11:01:11 +0100

    signup frontend done

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,29 +1,98 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./SignUpPage.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
 
 function SignUpPage() {
-    return (
-      <div className={styles["quiz-desires"]}>
-        <Link to="/">
-          <button className={styles["back-button"]}>Back</button>
-        </Link>
-        <div className={styles["main-content"]}>
-          <div className={styles["content"]}>
-            <h1 className={styles["header"]}>Almost done! Just one thing...</h1>
-            <h6>Please create an account to save your results:</h6>
-            <form className={styles["goals"]}>
-              <div className={styles["left"]}>
-                <label className={styles["job-title"]}>Name</label>
-                <div className={styles["custom-input"]}>
-                  <input type="text" className={styles["text-unit"]} />
-                </div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleName = (e) => setName(e.target.value);
+
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password, name };
+
+    axios
+      .post(`${API_URL}/auth/signup`, requestBody)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <div className={styles["quiz-desires"]}>
+      <Link to="/">
+        <button className={styles["back-button"]}>Back</button>
+      </Link>
+      <div className={styles["main-content"]}>
+        <img src="./login-woman.png" alt="happy woman" />
+        <div className={styles["content"]}>
+          <h1 className={styles["header"]}>Almost done! Just one thing...</h1>
+          <p>Please create an account to save your results:</p>
+          <form onSubmit={handleSignupSubmit} className={styles["goals"]}>
+            <div className={styles["left"]}>
+              <label  htmlFor="name" className={styles["job-title"]}>Name</label>
+              <div className={styles["custom-input"]}>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={name}
+                  onChange={handleName}
+                  className={styles["text-unit"]}
+                />
               </div>
-            </form>
-          </div>
+              <label htmlFor="email" className={styles["job-title"]}>Email</label>
+              <div className={styles["custom-input"]}>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={handleEmail}
+                  autoComplete="off"
+                  className={styles["text-unit"]}
+                />
+              </div>
+              <label  htmlFor="password" className={styles["job-title"]}>Password</label>
+              <div className={styles["custom-input"]}>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  autoComplete="current-password"
+                  onChange={handlePassword}
+                  className={styles["text-unit"]}
+                />
+              </div>
+              <button type="submit" className={styles["step"]}>
+                Sign up
+              </button>
+              <p>
+                Already have an account?<Link to={"/login"}> Log in here</Link>
+              </p>
+            </div>
+          </form>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
         </div>
       </div>
-    );
-  }
-  
-  export default SignUpPage;
+    </div>
+  );
+}
+
+export default SignUpPage;
