c9786b8310d2ecc3f78385e9cf529b2d37dae7e9
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-03-07 12:24:51 +0100

    added hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import App from "./App";
 import "./index.css";
 import { AuthProviderWrapper } from "./context/auth.context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+ <HashRouter>
       <AuthProviderWrapper>
         <App />
       </AuthProviderWrapper>
-    </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
